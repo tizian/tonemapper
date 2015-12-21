f19d279a5f995a20d8397ece0090b1ef2c585b9d
tizian <tizian.zeltner@gmail.com>
2015-12-21 16:09:36 +0100

    Add tonemapper selection

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,10 @@ class TonemapperScreen : public nanogui::Screen {
 public:
 	TonemapperScreen()
 		: nanogui::Screen(Eigen::Vector2i(1280, 960), "Tone Mapper", true, false) {
-		
-		m_tonemap = new ReinhardOperator(); // TODO: select with GUI
 
 		using namespace nanogui;
+
+		auto ctx = nvgContext();
 
 		glfwSetWindowPos(glfwWindow(), 20, 60);
 		setBackground(Vector3f(0.8f, 0.8f, 0.8f));
@@ -27,23 +27,120 @@ public:
 		m_window->setPosition(Vector2i(15, 15));
 		m_window->setLayout(new GroupLayout());
 
-		auto *openBtn = new Button(m_window, "Open image");
-		openBtn->setBackgroundColor(nanogui::Color(Vector3f(0.3f, 0.1f, 0.5f)));
+		auto about = new Button(m_window->buttonPanel(), "", ENTYPO_ICON_INFO);
+		about->setCallback([&, ctx] {
+			auto dlg = new MessageDialog(this, MessageDialog::Type::Information, "About Tone Mapper", "Info and description.\n\n(c) 2015 Tizian Zeltner");
+			dlg->messageLabel()->setFixedWidth(300);
+			dlg->messageLabel()->setFontSize(20);
+			performLayout(ctx);
+			dlg->center();
+		});
+
+		new Label(m_window, "Image I/O", "sans-bold");
+
+		auto *openBtn = new Button(m_window, "Open HDR image");
+		openBtn->setBackgroundColor(nanogui::Color(0, 255, 0, 25));
 		openBtn->setIcon(ENTYPO_ICON_FOLDER);
 		openBtn->setCallback([&] {
 			std::string filename = file_dialog({ {"exr", "OpenEXR"} }, false);
 			setImage(filename);
 		});
 
-		auto label = new Label(m_window, "Parameters", "sans-bold");
-		label->setFontSize(24.f);
+		auto *saveBtn = new Button(m_window, "Save LDR image");
+		saveBtn->setBackgroundColor(nanogui::Color(0, 255, 0, 25));
+		saveBtn->setIcon(ENTYPO_ICON_SAVE);
+		saveBtn->setCallback([&] {
+			std::string filename = file_dialog({ { "png", "Portable Network Graphics" } }, true);
+		});
+
+		new Label(m_window, "Tonemapping operator", "sans-bold");
+
+		m_tonemapSelection = new ComboBox(m_window, { "Gamma", "Reinhard" });
+		setTonemapper(new GammaOperator());
+		m_tonemapSelection->setCallback([&](int index) {
+			ToneMappingOperator *op = nullptr;
+			if (index == 0) {
+				op = new GammaOperator();
+			}
+			else if (index == 1) {
+				op = new ReinhardOperator();
+			}
+
+			if (op) {
+				setTonemapper(op);
+			}
+		});
+
+		drawAll();
+		setVisible(true);
+	}
+
+	void setImage(const std::string &filename) {
+		using namespace nanogui;
+		if (m_image) {
+			delete m_image;
+			m_image = nullptr;
+		}
+
+		m_image = new Image(filename);
+
+		if (m_image->getWidth() <= 0 || m_image->getHeight() <= 0) {
+			delete m_image;
+			m_image = nullptr;
+			return;
+		}
+
+		m_window->setPosition(Vector2i(15, 15));
+
+		m_scaledImageSize = Vector2i(MAIN_WIDTH, (MAIN_WIDTH * m_image->getHeight()) / m_image->getWidth());
+		m_windowSize = Vector2i(m_scaledImageSize.x(), m_scaledImageSize.y());
+
+		setSize(m_windowSize);
+		glfwSetWindowPos(glfwWindow(), 20, 100);
+
+		glGenTextures(1, &m_texture);
+		glBindTexture(GL_TEXTURE_2D, m_texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	}
+
+	void setTonemapper(ToneMappingOperator *tonemap) {
+		using namespace nanogui;
+
+		if (m_tonemap) {
+			delete m_tonemap;
+			m_tonemap = nullptr;
+		}
+
+		m_tonemap = tonemap;
+
+		MatrixXu indices(3, 2);
+		indices.col(0) << 0, 1, 2;
+		indices.col(1) << 2, 3, 0;
+
+		MatrixXf positions(2, 4);
+		positions.col(0) << 0, 0;
+		positions.col(1) << 1, 0;
+		positions.col(2) << 1, 1;
+		positions.col(3) << 0, 1;
+
+		m_tonemap->shader->bind();
+		m_tonemap->shader->uploadIndices(indices);
+		m_tonemap->shader->uploadAttrib("position", positions);
+
+		if (m_paramWidget) {
+			m_window->removeChild(m_paramWidget);
+		}
+
+		m_paramWidget = new Widget(m_window);
+		m_paramWidget->setLayout(new BoxLayout(Orientation::Vertical, Alignment::Minimum, 0, 10));
 
 		for (auto &parameter : m_tonemap->parameters) {
 			auto &p = parameter.second;
 
-			new Label(m_window, parameter.first, "sans-bold");
+			new Label(m_paramWidget, parameter.first, "sans-bold");
 
-			auto *panel = new Widget(m_window);
+			auto *panel = new Widget(m_paramWidget);
 			panel->setLayout(new BoxLayout(Orientation::Horizontal, Alignment::Middle, 0, 20));
 
 			auto toolButton = new ToolButton(panel, ENTYPO_ICON_CYCLE);
@@ -78,50 +175,6 @@ public:
 
 		// sidebar->setSize(Vector2i(SIDE_WIDTH, m_scaledImageSize.y()));
 		performLayout(mNVGContext);
-
-		drawAll();
-		setVisible(true);
-	}
-
-	void setImage(const std::string &filename) {
-		using namespace nanogui;
-		if (m_image) {
-			delete m_image;
-		}
-
-		m_image = new Image(filename);
-
-		if (m_image->getWidth() <= 0 || m_image->getHeight() <= 0) {
-			return;
-		}
-
-		m_window->setPosition(Vector2i(15, 15));
-
-		m_scaledImageSize = Vector2i(MAIN_WIDTH, (MAIN_WIDTH * m_image->getHeight()) / m_image->getWidth());
-		m_windowSize = Vector2i(m_scaledImageSize.x(), m_scaledImageSize.y());
-
-		setSize(m_windowSize);
-		setBackground(Vector3f(43 / 255.f, 43 / 255.f, 43 / 255.f));
-		glfwSetWindowPos(glfwWindow(), 20, 100);
-
-		MatrixXu indices(3, 2);
-		indices.col(0) << 0, 1, 2;
-		indices.col(1) << 2, 3, 0;
-
-		MatrixXf positions(2, 4);
-		positions.col(0) << 0, 0;
-		positions.col(1) << 1, 0;
-		positions.col(2) << 1, 1;
-		positions.col(3) << 0, 1;
-
-		m_tonemap->shader->bind();
-		m_tonemap->shader->uploadIndices(indices);
-		m_tonemap->shader->uploadAttrib("position", positions);
-
-		glGenTextures(1, &m_texture);
-		glBindTexture(GL_TEXTURE_2D, m_texture);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	}
 
 	virtual bool keyboardEvent(int key, int scancode, int action, int modifiers) {
@@ -169,9 +222,11 @@ private:
 	Image *m_image = nullptr;
     uint32_t m_texture = 0;
 
-    nanogui::Window *m_window;
+    nanogui::Window *m_window = nullptr;
+	nanogui::ComboBox *m_tonemapSelection = nullptr;
+	nanogui::Widget *m_paramWidget = nullptr;
 
-	ToneMappingOperator *m_tonemap;
+	ToneMappingOperator *m_tonemap = nullptr;
 
     const int MAIN_WIDTH = 960;
 	
